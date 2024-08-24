4bdf10ea7a0df21ea36f984f2d8bf502e9f84ffb
Ali Al-Kubaisi <60341470+alialkubaisi@users.noreply.github.com>
2024-08-24 15:21:14 +0200

    Fix mouse scrolling on message view

--- fxtend/src/main/java/io/fxtend/chatview/MessageView.java
+++ fxtend/src/main/java/io/fxtend/chatview/MessageView.java
@@ -1,6 +1,8 @@
 package io.fxtend.chatview;
 
+import javafx.application.Platform;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.VBox;
 
 public class MessageView extends ScrollPane
@@ -9,7 +11,7 @@ public class MessageView extends ScrollPane
 
     public MessageView()
     {
-        setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        setHbarPolicy(ScrollBarPolicy.NEVER);
         setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
         setFitToWidth(true);
         setMaxHeight(Double.MAX_VALUE);
@@ -19,8 +21,18 @@ public class MessageView extends ScrollPane
         messageViewContent.prefWidthProperty().bind(widthProperty());
         messageViewContent.getStyleClass().add("message-view-content");
 
-        vvalueProperty().bind(messageViewContent.heightProperty());
         setContent(messageViewContent);
+
+        // Enable mouse wheel and swipe scrolling
+        addEventFilter(ScrollEvent.SCROLL, event -> {
+            if (event.getDeltaY() != 0)
+            {
+                double delta = event.getDeltaY();
+                double newVValue = getVvalue() - delta / getContent().getBoundsInLocal().getHeight();
+                setVvalue(newVValue);
+                event.consume();
+            }
+        });
     }
 
     public void sendMessage(String message)
@@ -28,6 +40,7 @@ public class MessageView extends ScrollPane
         SendLabel sendLabel = new SendLabel(message);
         messageViewContent.getChildren().add(sendLabel);
         sendLabel.resizeMessageContainer(messageViewContent);
+        scrollToBottom();
     }
 
     public void receiveMessage(String message)
@@ -35,5 +48,11 @@ public class MessageView extends ScrollPane
         ReceiveLabel receiveLabel = new ReceiveLabel(message);
         messageViewContent.getChildren().add(receiveLabel);
         receiveLabel.resizeMessageContainer(messageViewContent);
+        scrollToBottom();
+    }
+
+    private void scrollToBottom()
+    {
+        Platform.runLater(() -> setVvalue(1.0));
     }
 }
